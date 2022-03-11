6100cc9eb9ad823991fd6c54766021021184e652
Ahmad Emami <emamiaed@gmail.com>
2022-03-11 14:37:24 +0330

    14001220 Homework02 firs edition
    Homework02 firs edition

---
+++ AhmadEmami_hw002_maktab71/HomeWork02-Q1/HomeWork02-Q1/Program.cs
@@ -0,0 +1,141 @@
+﻿Console.WriteLine("please enter interger array:");
+var input = Console.ReadLine();
+string[] inputArray = input.Split(',');
+for (int i = 0; i < inputArray.Length; i++)
+{
+    inputArray[i] = inputArray[i].Trim();
+}
+int[] inputNumber = new int[inputArray.Length];
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    inputNumber[i] = Convert.ToInt32(inputArray[i]);
+}
+Console.WriteLine("please select a method:");
+Console.WriteLine("1- Find Minimum");
+Console.WriteLine("2- Print Reverse Of Array");
+Console.WriteLine("3- Find Duplicate in Array");
+Console.WriteLine("4- Compair Two Array");
+int methodType = Convert.ToInt32(Console.ReadLine());
+switch (methodType)
+{
+    case 1:
+        Console.WriteLine(Min(inputNumber));
+        break;
+    case 2:
+        PrintArray(ReverseOfArray(inputNumber));
+        break;
+    case 3:
+        FindDuplicates(inputNumber);
+        break;
+    case 4:
+        CompairTowArray();
+        break;
+    default:
+        break;
+}
+//----------------------------------------------------------------------
+int Min(int[] arr)
+{
+    int min = arr[0];
+    foreach (var item in arr)
+    {
+        if (item < min)
+            min = item;
+    }
+    return min;
+}
+//--------------------------------------------------------------------
+int[] ReverseOfArray(int[] arr)
+{
+    int n = arr.Length;
+    int[] temp = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        temp[n - 1 - i] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = temp[i];
+    }
+    return arr; 
+}
+//--------------------------------------------------------------------
+void PrintArray(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write($"{item} - ");
+    }
+}
+//----------------------------------------------------------------------
+void FindDuplicates(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int count = 0, bl = 1;
+        for (int j = i; j < arr.Length; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (arr[i] == arr[j])
+            {
+                bl = 0;
+            }
+        }
+
+        if (bl == 1 && count >1)
+        {
+            Console.WriteLine("Item: "+arr[i] + "\t" + "Repeated: " + count);
+
+        }
+    }
+}
+//------------------------------------------------------------
+void CompairTowArray()
+{
+    Console.WriteLine("please enter another interger array:");
+    var input = Console.ReadLine();
+    string[] inputArray2 = input.Split(',');
+    for (int i = 0; i < inputArray2.Length; i++)
+    {
+        inputArray2[i] = inputArray2[i].Trim();
+    }
+    int[] inputNumber2 = new int[inputArray2.Length];
+
+    for (int i = 0; i < inputArray2.Length; i++)
+    {
+        inputNumber2[i] = Convert.ToInt32(inputArray2[i]);
+    }
+    int[] temp = new int[inputNumber.Length];
+    for (int i = 0; i < inputNumber.Length; i++)
+    {
+        int bl = 1;
+        for (int j = 0; j < inputNumber2.Length; j++)
+        {
+            if (inputNumber[i] == inputNumber2[j])
+            {
+                temp[i] = inputNumber2[j];
+            }
+        }
+        for (int j = i - 1; j >= 1; j--)
+        {
+            if (inputNumber[i] == inputNumber[j])
+            {
+                bl = 0;
+            }
+        }
+
+        if (bl == 1)
+        {
+            Console.Write(Convert.ToString(temp[i])+",");
+        }
+
+    }
+}
---
+++ AhmadEmami_hw002_maktab71/HomeWork02-Q2/HomeWork02-Q2/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Please enter a string of characters:");
+var str01 = Console.ReadLine();
+Console.WriteLine("Please enter another string of characters:");
+var str02 = Console.ReadLine();
+int len1 = str01.Length;
+int len2 = str02.Length;
+int finalLen = 0;
+if (len1 >= len2)
+{
+    finalLen = len1;
+}
+else
+{
+    finalLen = len2;
+}
+char[] ch01 = new char[finalLen];
+char[] ch02 = new char[finalLen];
+for (int i = 0; i < str01.Length; i++)
+{
+    ch01[i] = str01[i];
+}
+for (int i = 0; i < str02.Length; i++)
+{
+    ch02[i] = str02[i];
+}
+char[] temp = new char[ch02.Length];
+
+for (int i = 0; i < finalLen; i++)
+{
+    bool bl = true;
+    for (int j = 0; j < finalLen; j++)
+    {
+        if (ch01[i] == ch02[j])
+        {
+            temp[i] = ch02[j];
+        }
+    }
+    for (int j = i - 1; j >= 1; j--)
+    {
+        if (ch01[i] == ch01[j])
+        {
+            bl = false;
+        }
+    }
+
+    if (bl == true)
+    {
+        Console.Write(temp[i]);
+    }
+
+}
